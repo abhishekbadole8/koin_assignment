17512f98937e5c08fa6a44815edb3786410d1d69
Abhishek Badole <abhishekbadole8@gmail.com>
2024-03-05 17:56:22 +0530

    current coin section(hero) ready, right-container ready

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import styles from "./Hero.module.css";
+import bitcoin from "../../assets/bitcoin.png";
+import PercentageChange from '../PercentageChange/PercentageChange';
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className={styles.heroSection}>
+
+      {/* section header */}
+      <div className={styles.heroHeader}>
+
+        <div className={styles.currencyIcon}>
+          <img src={bitcoin} alt="curreny" />
+        </div>
+
+        <h3 className={styles.currencyTitle}>Bitcoin</h3>
+
+        <span className={styles.currencySymbol}>BTC</span>
+
+        <div className={styles.rankTag}>
+          <span className={styles.rankTagLabel}>Rank</span>
+          <span className={styles.rankTagLabel}>#1</span>
+        </div>
+
+      </div>
+
+      {/* body */}
+
+      <div className={styles.heroCurrencyPrice}>
+
+        <h2 className={styles.currencyPriceValue}>$46,953.04</h2>
+
+        <div className={styles.currencyPriceChange}>
+
+          <PercentageChange />
+
+          <span className={styles.currencyPriceChangePeriod}>(24H)</span>
+        </div>
+
+      </div>
+
+      <div className={styles.currencyPriceInINR}>
+        <p className={styles.currencyPriceInINRValue}>&#8377; 39,42,343</p>
+      </div>
+
+      {/* footer - chart */}
+      <div className={styles.heroChartContainer}>
+
+      </div>
+
+    </section>
   )
 }
 
---
+++ src/components/PercentageChange/PercentageChange.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./PercentageChange.module.css";
+
+function PercentageChange() {
+    return (
+        <div className={styles.currencyPriceChangePercentage}>
+            <span>
+                <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.5 0L11 8H0L5.5 0Z" fill="#14B079" />
+                </svg>
+            </span>
+            <p>2.51%</p>
+        </div>
+    )
+}
+
+export default PercentageChange
