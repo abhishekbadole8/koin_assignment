ff58667774bdcee8ddb47d19fb274b3365155a93
Abhishek Badole <abhishekbadole8@gmail.com>
2024-03-05 14:55:59 +0530

    Header-component ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Router>
     </>
 
   )
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import styles from "./Nav.module.css";
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav className={styles.nav}>
+
+      {/* Hamburger menu button for mobile */}
+      <div className={styles.hamburgerMenu}>
+        <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M0.0119629 2.80613V0.521484H20.5738V2.80613H0.0119629ZM0.0119629 8.51805H20.5738V6.2334H0.0119629V8.51805ZM0.0119629 14.2299H20.5738V11.9452H0.0119629V14.2299Z" fill="#0B182F" />
+        </svg>
+      </div>
+
+      {/* Navigation links */}
+      <a href="" className={styles.navItems}>Crypto Taxes</a>
+      <a href="" className={styles.navItems}>Free Tools</a>
+      <a href="" className={styles.navItems}>Resource Center</a>
+
+      {/* Get Started button */}
+      <div className={styles.startButton}>
+        <button>Get Started</button>
+      </div>
+    </nav>
   )
 }
 
